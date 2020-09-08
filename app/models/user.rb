f6f8d1dc751ff9e6e9b836dfa2035492f6a9dd62
rahalrazika <rahalraika@gmail.com>
2020-09-08 14:43:23 +0200

    update valdation in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true 
-    validates :email, presence: true 
-    validates :password, presence: true 
+    validates :username, presence: true,  uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true,  length: {5..20}
 end
