e360ccf471d3cdae74ddcda1daff6421df5525af
jacobrees <jacob.rees97@yahoo.com>
2020-09-08 13:33:15 +0100

    Add edit.html.erb to users view, Update router for user to handle edit and update requests sent from controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,21 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+    
+        if @user.save
+          redirect_to new_user_path
+        else
+          render :edit
+        end
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password, :id)
