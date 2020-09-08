9b255b946573dd2dfc66e83b55e08c88e42acd03
rahalrazika <rahalraika@gmail.com>
2020-09-08 15:02:45 +0200

    add the errors flash the new.html.erb and edit.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,6 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
-      
         if @user.save
           redirect_to new_user_path
         else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true,  uniqueness: true
     validates :email, presence: true, uniqueness: true
-    validates :password, presence: true,  length: {5..20}
+    validates :password, presence: true,  length: { in: 5..20 }
 end
